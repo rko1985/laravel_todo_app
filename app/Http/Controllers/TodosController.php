c758c4b7bd1bbd3ab33d5d51b510aef4d939ba63
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-08-06 11:13:15 -1000

    reading from database

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodosController extends Controller
 {
+    
     public function index(){
-        return view('todos.index');
+        //fetch all todos from database and display them in the todos.index page
+
+        return view('todos.index')->with('todos', Todo::all());
     }
 }
